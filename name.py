e2df9a295f1c81e75fbc2cb703d57dd5c1899ffd
samridhikansal <kansal.samridhi@gmail.com>
2024-12-23 22:12:18 -0700

    checking if the number is even odd or a zero

--- name.py
+++ name.py
@@ -1,9 +1,7 @@
-name = input("What is your name?") 
-num = input("What is your favorite number? ")
-print("")
-
-# Uses user input to print out information
-print("Hello," , name)
-print("my favorite number is " , num )
-print("") 
-print(type(name))
+myNumber = input ("Enter a number: ")
+if myNumber > 0:
+    print ("your number is positive")
+elif myNumber ==0:
+    print("your number is zero")
+else:
+    print("your number is negative")
