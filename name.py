88c38d224aaddca15a862339774d31a6331de3bc
samridhikansal <kansal.samridhi@gmail.com>
2024-12-23 21:53:15 -0700

    file changed

--- name.py
+++ name.py
@@ -1,1 +1,9 @@
-print("Hello World")
+name = input("What is your name?") 
+num = input("What is your favorite number? ")
+print("")
+
+# Uses user input to print out information
+print("Hello," , name)
+print("my favorite number is " , num )
+print("") 
+print(type(name))
